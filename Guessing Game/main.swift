0b55d41fed9a477f39ee9a9e8a893c3703fc829c
Devin Cook <Devin.Cook1901@gmail.com>
2018-01-23 16:27:33 -0500

    initial commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Devin Cook on 1/23/18.
+//  Copyright © 2018 Devin Cook. All rights reserved.
+//
+
+import Foundation
+
+//Generate a random number
+//Ask user input
+//Take user input
+//Determine wrong or right(Loop until right)
+//If correct ask if they want to play again
+//If incorrect tell them the correct answer and ask them to play
+
+var randomNumber = arc4random_uniform(100)
+
