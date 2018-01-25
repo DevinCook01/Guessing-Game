5e0a8bbff9e0f92de45fba16ce3bb594eb6ae993
Devin Cook <Devin.Cook1901@gmail.com>
2018-01-25 13:25:21 -0500

    Added user input & higher/lower output

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -9,11 +9,27 @@
 import Foundation
 
 //Generate a random number
+var randomNumber = Int(arc4random_uniform(100))
+var guess : Int?
 //Ask user input
+print("Please guess a number between 1 and 100. You have 5 guesses.")
 //Take user input
+for _ in 0...4{
+    guess = Int(readLine()!)!
+    if randomNumber == guess! {
+    print("Winner! 👏")
+        break
+} else if randomNumber < guess! {
+    print("Lower 👇")
+} else {
+    print("Higher 👆")
+}}
+print("The actual answer was \(randomNumber)")
 //Determine wrong or right(Loop until right)
+
 //If correct ask if they want to play again
+
 //If incorrect tell them the correct answer and ask them to play
 
-var randomNumber = arc4random_uniform(100)
+
 
